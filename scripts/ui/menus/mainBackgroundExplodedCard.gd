3137a8341d2a11a941ff08c94cdef8eceff1a837
kylejussab <kylejussab@gmail.com>
2026-06-29 10:31:04 -0700

    added 4 more factions to exploding card art

--- scripts/ui/menus/mainBackgroundExplodedCard.gd
+++ scripts/ui/menus/mainBackgroundExplodedCard.gd
@@ -1,17 +1,42 @@
 extends Control
 
-@onready var layers: Array = [
-	$UI,
-	$"Clicker 1", $"Clicker 2", $"Clicker 3", $"Clicker 4", $"Clicker 5", $"Clicker 6", $"Clicker 7",
-	$"Background 1", $"Background 2", $"Background 3", $"Background 4", $"Background 5",
-]
+@onready var explodedCards = {
+	"Firefly": {
+		"Sprites": [$"Firefly/Marlene 1", $"Firefly/Marlene 2", $"Firefly/Marlene 3", $"Firefly/Marlene 4", $"Firefly/Marlene 5", $"Firefly/Background 1", $"Firefly/Background 2", $"Firefly/Background 3", $"Firefly/Background 4", $"Firefly/Background 5"],
+		"DefaultPositions": [380, 405, 430, 455, 480, 630, 655, 680, 705, 730],
+		"CharacterExplodedPositions": [300, 420, 540, 660, 780, 930, 940, 960, 970, 980],
+		"BackgroundExplodedPositions": [150, 160, 170, 180, 190, 380, 480, 580, 680, 780]
+	},
+	"Infected": {
+		"Sprites": [$"Infected/Clicker 1", $"Infected/Clicker 2", $"Infected/Clicker 3", $"Infected/Clicker 4", $"Infected/Clicker 5", $"Infected/Clicker 6", $"Infected/Clicker 7", $"Infected/Background 1", $"Infected/Background 2", $"Infected/Background 3", $"Infected/Background 4", $"Infected/Background 5"],
+		"DefaultPositions": [355, 380, 405, 430, 455, 480, 505, 655, 680, 680, 705, 730],
+		"CharacterExplodedPositions": [180, 300, 420, 540, 660, 780, 900, 1050, 1060, 1060, 1070, 1080],
+		"BackgroundExplodedPositions": [30, 40, 50, 60, 70, 80, 90, 480, 580, 580, 680, 780]
+	},
+	"Jackson": {
+		"Sprites": [$"Jackson/Tommy 1", $"Jackson/Tommy 2", $"Jackson/Tommy 3", $"Jackson/Tommy 4", $"Jackson/Tommy 5", $"Jackson/Background 1", $"Jackson/Background 2", $"Jackson/Background 3", $"Jackson/Background 4", $"Jackson/Background 5"],
+		"DefaultPositions": [380, 405, 430, 455, 480, 630, 655, 680, 705, 730],
+		"CharacterExplodedPositions": [300, 420, 540, 660, 780, 930, 940, 960, 970, 980],
+		"BackgroundExplodedPositions": [150, 160, 170, 180, 190, 380, 480, 580, 680, 780]
+	},
+	"Seraphite": {
+		"Sprites": [$"Seraphite/Foreground 1", $"Seraphite/Yara 1", $"Seraphite/Yara 2", $"Seraphite/Yara 3", $"Seraphite/Yara 4", $"Seraphite/Yara 5", $"Seraphite/Background 1", $"Seraphite/Background 2", $"Seraphite/Background 3", $"Seraphite/Background 4", $"Seraphite/Background 5"],
+		"DefaultPositions": [230, 380, 405, 430, 455, 480, 630, 655, 680, 705, 730],
+		"CharacterExplodedPositions": [50, 300, 420, 540, 660, 780, 930, 940, 960, 970, 980],
+		"BackgroundExplodedPositions": [140, 150, 160, 170, 180, 190, 380, 480, 580, 680, 780]
+	},
+	"WLF": {
+		"Sprites": [$"WLF/Abby 1", $"WLF/Abby 2", $"WLF/Abby 3", $"WLF/Abby 4", $"WLF/Abby 5", $"WLF/Background 1", $"WLF/Background 2", $"WLF/Background 3", $"WLF/Background 4", $"WLF/Background 5"],
+		"DefaultPositions": [380, 405, 430, 455, 480, 630, 655, 680, 705, 730],
+		"CharacterExplodedPositions": [300, 420, 540, 660, 780, 930, 940, 960, 970, 980],
+		"BackgroundExplodedPositions": [150, 160, 170, 180, 190, 380, 480, 580, 680, 780]
+	},
+}
 
 @onready var characterHitbox: Control = $CharacterHitbox
 @onready var backgroundHitbox: Control = $BackgroundHitbox
 
-var defaultYPositions: Array = [205, 355, 380, 405, 430, 455, 480, 505, 655, 680, 680, 705, 730]
-var characterExplodedYPositions: Array = [30, 180, 300, 420, 540, 660, 780, 900, 1050, 1060, 1060, 1070, 1080]
-var backgroundExplodedYPositions: Array = [5, 30, 40, 50, 60, 70, 80, 90, 480, 580, 580, 680, 780]
+var selectedFaction: String
 
 var activeTween: Tween
 var currentState: String = "default"
@@ -21,6 +46,18 @@ const MICRO_FAR := 35.0
 const MICRO_SPEED := 10.0
 
 func _ready() -> void:
+	var factions = explodedCards.keys() 
+	selectedFaction = factions.pick_random()
+	
+	for factionName in factions:
+		var factionNode = get_node_or_null(factionName)
+		if factionNode:
+			factionNode.hide()
+			
+	var selectedNode = get_node_or_null(selectedFaction)
+	if selectedNode:
+		selectedNode.show()
+	
 	$Darkener.modulate.a = 0.5
 	set_process(false)
 
@@ -47,8 +84,8 @@ func _tween_to_state(state: String) -> void:
 			set_process(false)
 			activeTween = create_tween().set_parallel(true).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
 			activeTween.tween_property($Darkener, "modulate:a", 0.5, 0.25)
-			for i in range(layers.size()):
-				activeTween.tween_property(layers[i], "position:y", defaultYPositions[i], 0.25)
+			for i in range(explodedCards[selectedFaction]["Sprites"].size()):
+				activeTween.tween_property(explodedCards[selectedFaction]["Sprites"][i], "position:y", explodedCards[selectedFaction]["DefaultPositions"][i], 0.25)
 		
 		"character", "background":
 			set_process(true)
@@ -76,14 +113,14 @@ func _process(deltaTime: float) -> void:
 	
 	match currentState:
 		"character":
-			explodedPositions = characterExplodedYPositions
+			explodedPositions = explodedCards[selectedFaction]["CharacterExplodedPositions"]
 			activeHitbox = characterHitbox
 			indexMinimum = 1
 			indexMaximum = 7
 			focusMinimum = 1.0
 			focusMaximum = 7.0
 		"background":
-			explodedPositions = backgroundExplodedYPositions
+			explodedPositions = explodedCards[selectedFaction]["BackgroundExplodedPositions"]
 			activeHitbox = backgroundHitbox
 			indexMinimum = 8
 			indexMaximum = 12
@@ -95,9 +132,9 @@ func _process(deltaTime: float) -> void:
 	var verticalRatio = clamp((mouseGlobalY - activeHitbox.global_position.y) / activeHitbox.size.y, 0.0, 1.0)
 	var dynamicFocus := lerpf(focusMinimum, focusMaximum, verticalRatio)
 	
-	for i in range(layers.size()):
+	for i in range(explodedCards[selectedFaction]["Sprites"].size()):
 		var targetPositionY := float(explodedPositions[i])
 		if i >= indexMinimum and i <= indexMaximum:
 			var signedDistance := float(i) - dynamicFocus
 			targetPositionY += sign(signedDistance) * _micro_push(abs(signedDistance))
-		layers[i].position.y = lerpf(layers[i].position.y, targetPositionY, MICRO_SPEED * deltaTime)
+		explodedCards[selectedFaction]["Sprites"][i].position.y = lerpf(explodedCards[selectedFaction]["Sprites"][i].position.y, targetPositionY, MICRO_SPEED * deltaTime)
